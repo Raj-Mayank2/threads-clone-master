c583ebaf1ee4bb6f6ec8f80c97d49fabd84b1a5e
Mayank Raj <153426642+Raj-Mayank2@users.noreply.github.com>
2025-01-20 14:32:16 +0530

    Update vite.config.js

--- threads-clone-master/frontend/vite.config.js
+++ threads-clone-master/frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
 		// Get rid of the CORS error
 		proxy: {
 			"/api": {
-				target: "https://threads-clone-master-backend-374n.onrender.com",
+				target: "http://localhost:5000",
 				changeOrigin: true,
 				secure: false,
 			},
